ed359205078c52dbf02fdb750f769adb5be4d047
Justin Hardin <jpmitchell293@gmail.com>
2015-09-27 15:46:44 -0700

    added albums

--- src/app/artist/artist.controller.js
+++ src/app/artist/artist.controller.js
@@ -8,5 +8,14 @@
   /** @ngInject */
   function ArtistController() {
     var vm = this;
+
+    vm.selectedMode = 'md-fling';
+
+    vm.albums = [
+      { title: 'Creature Dreams - EP', cover: '../../assets/images/mz.jpg'},
+      { title: 'Half Shadows', cover: '../../assets/images/tokimonsta_halfshadows.jpg'},
+      { title: 'Cosmic Intoxication', cover: '../../assets/images/cosmic-intoxica.jpg'},
+    ];
+
   }
 })();
